5a4645642d7c40eedd25c576a56f0ec4d7cdc138
Keyairra Wright <keyairra.wright@gmail.com>
2018-10-03 11:20:53 -0400

    connecting facts to the game page

--- Assets/myScripts/GameManagement.cs
+++ Assets/myScripts/GameManagement.cs
@@ -1,13 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameManagement : MonoBehaviour {
 
-    public Question[] questions;
+    public Question[] questions; //below will allow us to pick random questions w/o repeating
+    private static List<Question> unansweredQuestions;
+
+    private Question currentQuestion;
+
+    [SerializeField]
+    private Text factText;
 
     private void Start()
     {
+        if(unansweredQuestions == null || unansweredQuestions.Count == 0){
+            unansweredQuestions = questions.ToList<Question>();
+        }
 
+        SetCurrentQuestion();
+
+    }
+
+    void SetCurrentQuestion(){ //below will take the element from the random index
+        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+        factText.text = currentQuestion.fact;
+        unansweredQuestions.RemoveAt(randomQuestionIndex); //this removes questions from the list once they are answered
     }
 }

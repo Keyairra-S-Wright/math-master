9768197d0a8812a6b4b8396cc21b057ca683dc69
Keyairra Wright <keyairra.wright@gmail.com>
2018-10-03 21:03:08 -0400

    linking up functionality to end the Game

--- Assets/myScripts/Score.cs
+++ Assets/myScripts/Score.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
@@ -9,19 +10,28 @@ public class Score : MonoBehaviour
     public Text scoreText;
 
     public static int scoreValue = 0;
+    public int endScore = 100;
     Text score;
 
     void Start()
     {
         score = GetComponent<Text>();
         scoreText.text = "";
-
         Debug.Log(score);
+        if (scoreValue >= endScore) 
+        {
+            EndGame();
+        }
     }
 
     void Update()
     {
         scoreText.text = "Score: " + scoreValue.ToString();
+    }
+
+    void EndGame()
+    {
+        SceneManager.LoadScene("EndingScene");
     }
 
 }
